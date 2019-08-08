94082da70c04eba751517b75924d8910ab7d0a9a
zhongzhongm <zhongzhongm.@yonyou.com>
2019-08-08 16:42:31 +0800

    创建用户表

---
+++ src/main/resources/SQL/create_table_mysql.sql
@@ -0,0 +1,20 @@
+####################
+--	用户
+####################
+-- ----------------------------
+-- Table structure for zz_user
+-- ----------------------------
+DROP TABLE IF EXISTS `zz_user`;
+CREATE TABLE `zz_user` (
+	`id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+	`code` varchar(20) COMMENT '账号',
+	`name` varchar(255) COMMENT '用户名称',	
+	`password` 	varchar(32) COMMENT '密码',	
+	`age` int(100) COMMENT '年龄',
+	`create_date` date COMMENT '创建日期',	
+	`modify_date` date COMMENT '修改日期',
+	`creator` varchar(20) COMMENT '创建人',	
+	`sex` int(2) COMMENT '性别',
+	`address` varchar(100) COMMENT '地址'	,	
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8;
